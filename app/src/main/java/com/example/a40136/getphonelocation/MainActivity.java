30a902119c8906840950eea79ee62f4044575ef0
ljsdyspg <issljsdyspg@163.com>
2018-09-19 20:50:18 +0800

    修改位置

--- app/src/main/java/com/example/a40136/getphonelocation/MainActivity.java
+++ app/src/main/java/com/example/a40136/getphonelocation/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
@@ -18,45 +19,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button button;
     private TextView textView;
     private LocationManager locationManager;
-    private String provider;
+    private String locationProvider;       //位置提供器
     private String locationInfo;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initUI();
-
-        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        //获取当前可用的位置控制器
-        List<String> list = locationManager.getProviders(true);
-
-        if (list.contains(LocationManager.GPS_PROVIDER)) {
-            //是否为GPS位置控制器
-            provider = LocationManager.GPS_PROVIDER;
-        }
-        else if (list.contains(LocationManager.NETWORK_PROVIDER)) {
-            //是否为网络位置控制器
-            provider = LocationManager.NETWORK_PROVIDER;
-
-        } else {
-            Toast.makeText(this, "请检查网络或GPS是否打开",
-                    Toast.LENGTH_LONG).show();
-            return;
-        }
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        if (location != null) {
-            //获取当前位置，这里只用到了经纬度
-            locationInfo = "纬度为：" + location.getLongitude() + ",经度为："
-                    + location.getLatitude();
-            Toast.makeText(this,"已经赋值",Toast.LENGTH_SHORT).show();
-        }
-
+        getLocation(this);
 
     }
-
-
     private void initUI(){
 
         button=(Button)findViewById(R.id.button);
@@ -65,6 +38,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button.setOnClickListener(this);
     }
 
+    private void getLocation(Context context){
+        //1.获取位置管理器
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        //2.获取位置提供器，GPS或是NetWork
+        List<String> providers = locationManager.getProviders(true);
+        if (providers.contains(LocationManager.NETWORK_PROVIDER)){
+            //如果是网络定位
+            locationProvider = LocationManager.NETWORK_PROVIDER;
+        }else if (providers.contains(LocationManager.GPS_PROVIDER)){
+            //如果是GPS定位
+            locationProvider = LocationManager.GPS_PROVIDER;
+        }else {
+            Toast.makeText(this, "没有可用的位置提供器", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //3.获取上次的位置，一般第一次运行，此值为null
+        Location location = locationManager.getLastKnownLocation(locationProvider);
+        if (location!=null){
+            showLocation(location);
+        }else{
+            // 监视地理位置变化，第二个和第三个参数分别为更新的最短时间minTime和最短距离minDistace
+            locationManager.requestLocationUpdates(locationProvider, 0, 0,mListener);
+        }
+    }
+
+    private void showLocation(Location location){
+        locationInfo = "纬度："+location.getLatitude()+"经度："+location.getLongitude();
+        textView.setText(locationInfo);
+    }
+
+    LocationListener mListener = new LocationListener() {
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+        // 如果位置发生变化，重新显示
+        @Override
+        public void onLocationChanged(Location location) {
+            showLocation(location);
+        }
+    };
     @Override
     public void onClick(View view) {
         textView.setText(locationInfo);
